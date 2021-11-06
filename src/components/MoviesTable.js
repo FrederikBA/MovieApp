0071af3960dbcbbd22a17ed3451b5940def096fa
FrederikBA <54848880+FrederikBA@users.noreply.github.com>
2021-11-06 21:37:55 +0100

    add sort to table

--- src/components/MoviesTable.js
+++ src/components/MoviesTable.js
@@ -1,9 +1,9 @@
 import { useState, useEffect } from 'react'
 import star from '../img/star.png'
 
-
 const MoviesTable = ({ apiFacade }) => {
     const [movies, setMovies] = useState([]);
+    const [isSorted, setIsSorted] = useState("unsorted");
 
     useEffect(() => {
         const getMovies = async () => {
@@ -13,20 +13,49 @@ const MoviesTable = ({ apiFacade }) => {
         getMovies()
     }, []);
 
+    const sortById = () => {
+        setIsSorted("byid")
+        const sorted = (movies.sort((a, b) => { return a.id - b.id }))
+        setMovies(sorted)
+    }
+
+    const sortByYear = () => {
+        setIsSorted("byyear")
+        const sorted = (movies.sort((a, b) => { return a.year - b.year }))
+        setMovies(sorted)
+    }
+
+    const sortByTitle = () => {
+        setIsSorted("bytitle")
+        const sorted = (movies.sort((a, b) => { return (a.title < b.title) ? -1 : (a.title > b.title) ? 1 : 0; }))
+        console.log(sorted);
+        setMovies(sorted)
+    }
+
+    const sortByRating = () => {
+        setIsSorted("byrating")
+        const sorted = (movies.sort((a, b) => { return a.rating - b.rating }))
+        setMovies(sorted)
+
+    }
+
     return (
         <div>
             <table className="table table-dark movieTable">
                 <thead className="mt-head">
-                    <th>Year</th>
-                    <th>Title</th>
-                    <th>IMDb Rating</th>
-                    <th>IMDb</th>
+                    <tr>
+                        <th className="mt-head-title" onClick={sortById}>ID</th>
+                        <th className="mt-head-title" onClick={sortByYear}>Year</th>
+                        <th className="mt-head-title" onClick={sortByTitle}>Title</th>
+                        <th className="mt-head-title" onClick={sortByRating}>IMDb Rating</th>
+                        <th>IMDb</th>
+                    </tr>
                 </thead>
                 <tbody>
-                    {movies.map((movie) => <tr><td>{movie.year}</td><td>{movie.title}</td><td><img className="star" src={star} />{movie.rating}/10</td><td><a className="imdbLink" href={movie.imdb} rel="noopener noreferrer" target="_blank">IMDb</a></td> </tr>)}
+                    {movies.map((movie) => <tr key={movie.id}><td>{movie.id}</td><td>{movie.year}</td><td>{movie.title}</td><td><img className="star" src={star} alt="Rating Star" />{movie.rating}/10</td><td><a className="imdbLink" href={movie.imdb} rel="noopener noreferrer" target="_blank">IMDb</a></td></tr>)}
                 </tbody>
             </table>
-        </div>
+        </div >
     )
 }
 
