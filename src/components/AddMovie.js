dc5f653b1fbeb7d7aecc82812be95f16b26be5e5
FrederikBA <54848880+FrederikBA@users.noreply.github.com>
2021-11-07 15:59:20 +0100

    add errorhandling @AddMovie

--- src/components/AddMovie.js
+++ src/components/AddMovie.js
@@ -1,23 +1,33 @@
 import { useState } from "react";
 
 const AddMovie = ({ apiFacade }) => {
-    const init = { year: 0, title: "", imdb: "", rating: 0 };
+    const init = { year: "", title: "", imdb: "", rating: "" };
     const [movie, setMovie] = useState(init);
+    const [statusMessage, setStatusMessage] = useState("");
+    const [msgColor, setMsgColor] = useState("");
 
     const handleInput = (event) => {
         setMovie({ ...movie, [event.target.id]: event.target.value })
     }
 
     const handleClick = (event) => {
-        event.preventDefault()
         apiFacade.addMovie(movie.year, movie.title, movie.imdb, movie.rating)
+            .catch(error => {
+                if (error.response) {
+                    setMsgColor('#FF0000');
+                    setStatusMessage(error.response.data.message);
+                }
+            })
+        setMsgColor('#4caf50');
+        setStatusMessage('Movie Added Successfully!');
+        event.preventDefault()
     }
-
 
 
     return (
         <div className="addMovie">
-            <h1>AddMovie</h1>
+            <h2>Add Movie</h2>
+            <p className="statusMsg" style={{ color: msgColor }}>{statusMessage}</p>
             <form onChange={handleInput} className="form-group">
                 <input className="form-control addInput" id="year" placeholder="Enter year" type="text"></input>
                 <input className="form-control addInput" id="title" placeholder="Enter title" type="text"></input>
